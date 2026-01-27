b8bbc5e1182a1bef6281706888f5c1a6bb7c9950
akurfurst <219035144+akurfurst@users.noreply.github.com>
2026-01-27 19:43:45 +0000

    Added bug jutter and the ability for the user to add more bugs on the screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_sprite_ptr.h>
+#include <bn_random.h>
+#include <bn_vector.h>
+#include <bn_keypad.h>
+
 #include "bn_sprite_items_bug.h"
 
 
@@ -9,11 +13,27 @@
 int main(){
     bn::core::init();
 
+    bn::random rng = bn::random();
+
     bn::backdrop::set_color(bn::color(16,16,31));
 
-    bn::sprite_ptr bug = bn::sprite_items::bug.create_sprite();
+    bn::vector<bn::sprite_ptr, 50> bugs = {};
+
+    //bn::sprite_ptr bug = bn::sprite_items::bug.create_sprite();
 
     while(true){
+        for(bn::sprite_ptr& bug: bugs){
+            bn::fixed new_x = bug.x() + rng.get_fixed(-1, 1);
+            bn::fixed new_y = bug.y() + rng.get_fixed(-1, 1);
+            bug.set_position(new_x, new_y);
+        }
+        
+
+        if (bn::keypad::a_pressed()){
+            bugs.push_back(bn::sprite_items::bug.create_sprite());
+        }
+
+        rng.update();
         bn::core::update();
     }
 }
