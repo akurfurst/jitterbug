44008717dbcf1ab2bcb4624a14560cdf74d907aa
akurfurst <219035144+akurfurst@users.noreply.github.com>
2026-01-27 19:01:37 +0000

    Added sprite to screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <bn_core.h>
 #include <bn_backdrop.h>
 #include <bn_color.h>
+#include <bn_sprite_ptr.h>
+#include "bn_sprite_items_bug.h"
 
 
 
@@ -8,6 +10,8 @@ int main(){
     bn::core::init();
 
     bn::backdrop::set_color(bn::color(16,16,31));
+
+    bn::sprite_ptr bug = bn::sprite_items::bug.create_sprite();
 
     while(true){
         bn::core::update();
